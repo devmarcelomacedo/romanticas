af6768e2c9d0de73ebf885cf5ada19e220d50225
devmarcelomacedo <devmarcelomacedo@gmail.com>
2023-10-07 12:10:48 -0300

    adicionando musicas

--- songs.js
+++ songs.js
@@ -2,87 +2,86 @@ export default [
 {
     src: "./src/Ponte Sobre Águas Turva.mp3",
     name: "Ponte Sobre Águas Turva - Marco Aureli",
-    name: " ",
 },
 {
-    src: "./",
-    name: " ",
+    src: "./src/Sergio Lopes   o rei e o ladrã.mp3",
+    name: "o rei e o ladrão- Sergio Lopes ",
 },
 {
-    src: "./",
+    src: "./src/Sérgio Lopes   Tente Lembra.mp3",
+    name: "Tente Lembra - Sergio lopes",
+},
+{
+    src: "./src/Recebi um Novo Coração do Pai   Não é Aline Barros   Legendad.mp3",
+    name: "Recebi um Novo Coração do Pai - Aine Barros",
+},
+{
+    src: "./src/SÉRGIO LOPES.mp3",
+    name: "Sergio Lopes",
+},
+{
+    src: "./src/Situaçõe.mp3",
     name: "",
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/Sonda M.mp3",
+    name: "Sonda -me - Aline Barros",
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/Sou Grato Ao Senho.mp3",
+    name: "Sou Grato Ao Senho",
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/Última Lágrima   Gerson Cardoz.mp3",
+    name: "Última Lágrima - Gerson Cardoz",
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/Verdadeiro Adorado.mp3",
+    name: "Verdadeiro Adorado",
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/Meu Univers.mp3",
+    name: "Meu Universo",
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/O Sonho de José   Mattos Nascimento [MATTOS EM CASA.mp3",
+    name: "O Sonho de José - Matos Nascimento",
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/src/O Lamento de Israel   Sérgio Lope.mp3 Sérgio Lope.mp3",
+    name: "O Lamento de Israel   Sérgio Lope.mp3", 
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/Nenhuma condenação h�.mp3",
+    name: "Nenhuma condenação", 
 },
 {
-    src: "./",
-    name: "",
+    src: "./src/Não Há Barreira.mp3",
+    name: "Não Há Barreira", 
 },
 {
-    src: "./",
-    name: "", 
+    src: "./srcsrc/Olha o Ma.mp3.mp3",
+    name: "Olha o Mar.mp3", 
 },
 {
-    src: "./",
-    name: "", 
+    src: "./src/GABRIELA ROCHA   DIZ (YOU SAY) (LYRIC VÍDEO.mp3",
+    name: "DIZ ", 
 },
 {
-    src: "./",
-    name: "", 
+    src: "./src/Eu Te Agradeç.mp3",
+    name: "Eu Te Agradeço",
 },
 {
-    src: "./",
-    name: " ", 
+    src: "./src/Gerson Cardoso   Eis me aqui no teu alta.mp3",
+    name: "Eis me aqui no teu altar",
 },
 {
-    src: "./",
-    name: "", 
+    src: "./src/Deus Soberan.mp3",
+    name: "Deus Soberano",
 },
 {
-    src: "./",
-    name: "",
-},
-{
-    src: "./",
-    name: "",
-},
-{
-    src: "./",
-    name: "",
-},
-{
-    src: "./",
-    name: "",
+    src: "./src/Adorador por Excelênci.mp3",
+    name: "Adorador por Excelência",
 },
 {
     src: "./",
